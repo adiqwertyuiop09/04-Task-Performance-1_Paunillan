4cf0f177ce3269741b54b30b38912e02aaac1b69
adiqwertyuiop.09 <adiqwertyuiop.09@gmail.com>
2025-09-29 19:00:43 +0800

    Add project files.

---
+++ frmTrackThread/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace frmTrackThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnrun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class MyThreadClass
+        {
+            public static void Thread1()
+            {
+                Thread thread = Thread.CurrentThread;
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(thread.Name + " prints number " + i);
+                    Thread.Sleep(1500);
+                }
+            }
+
+            public static void Thread2() 
+            {
+                
+            }
+        }
+    }
+}
